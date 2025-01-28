830b459a27a1ae53463b44f5eebe511d94e98281
Amy X. Lu <lu.amy326@gmail.com>
2025-01-27 22:31:55 -0800

    generate entry point evo2_7b_gen

---
+++ generate.sh
@@ -0,0 +1,4 @@
+# evo2_7b_gen
+python generate.py \
+    --checkpoint_path /scratch/hielab/gbrixi/evo2/vortex_interleaved/7b/iter_500000.pt \
+    --config_path /scratch/hielab/gbrixi/evo2/vortex_interleaved/7b/shc-evo2-7b-8k-2T-v2-interleave.yml
