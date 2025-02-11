830fa64214d4771dccb8bac106d75f45d89d43c4
Amy X. Lu <lu.amy326@gmail.com>
2025-02-11 15:17:31 -0800

    Update checkpoint loading

--- speculative_generate.sh
+++ speculative_generate.sh
@@ -1,4 +1,4 @@
 # evo2_7b_gen
-python -m pdb speculative_generate.py \
+python speculative_generate.py \
     --checkpoint_path /scratch/hielab/gbrixi/evo2/vortex_interleaved/7b/iter_500000.pt \
     --config_path /scratch/hielab/gbrixi/evo2/vortex_interleaved/7b/shc-evo2-7b-8k-2T-v2-interleave.yml
